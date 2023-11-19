6905f368e28f2e963652f2a4dcd7421eaa9d31bf
Bernard R <bernard.r@travauxef.fr>
2023-11-19 13:20:16 +0100

    Étape 1: Thème personnalisé

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
 <footer>
-  
+  <?php wp_nav_menu(array(
+    'theme_location' => 'footer-menu', // Utilisez le nom de l'emplacement du menu déclaré dans votre thème
+    'menu_class' => 'footer-menu', // Classe CSS pour le menu
+  )); ?>
 </footer>
 <?php wp_footer(); ?>
 </body>
--- functions.php
+++ functions.php
@@ -2,8 +2,11 @@
 // Enqueue des styles CSS et scripts JS
 function enqueue_motaphoto_styles_and_scripts()
 {
-  wp_enqueue_style('motaphoto-style', get_stylesheet_uri());
-  //wp_enqueue_script('motaphoto-scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0', true);
+  wp_enqueue_style('motaphoto-style', get_stylesheet_uri(), array(), false);
+  wp_enqueue_style('motaphoto-header', get_stylesheet_directory_uri() . '/assets/css/header.css', array('motaphoto-style'), false);
+  wp_enqueue_style('motaphoto-footer', get_stylesheet_directory_uri() . '/assets/css/footer.css', array('motaphoto-style'), false);
+  
+  wp_enqueue_script('motaphoto-scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0', true);
 }
 add_action('wp_enqueue_scripts', 'enqueue_motaphoto_styles_and_scripts');
 
@@ -31,24 +34,3 @@ function custom_logo_setup()
   add_theme_support('custom-logo', $defaults);
 }
 add_action('after_setup_theme', 'custom_logo_setup');
-
-// Gestion de la modale de contact
-function display_contact_modal()
-{
-  
-}
-add_action('wp_footer', 'display_contact_modal');
-
-// Fonction pour réduire la taille des images téléchargées
-function custom_image_sizes()
-{
-  
-}
-add_action('after_setup_theme', 'custom_image_sizes');
-
-// Désactiver les requêtes API de WordPress non nécessaires
-function disable_wp_api()
-{
-  // Désactivez ici les requêtes API de WordPress non nécessaires
-}
-add_action('init', 'disable_wp_api');
